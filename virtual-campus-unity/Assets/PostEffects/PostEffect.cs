ad299dcf9285cbcd096b5cd29558060de9f93a35
Tymon <ftm2001@163.com>
2020-04-05 22:11:11 +0800

    add asset

---
+++ virtual-campus-unity/Assets/PostEffects/PostEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class PostEffect : MonoBehaviour
+{
+	protected Material CheckShaderAndCreateMaterial(Shader shader, Material material)
+	{
+		if (shader == null || !shader.isSupported)
+		{
+			return null;
+		}
+		if (shader.isSupported && material && material.shader == shader)
+		{
+			return material;
+		}
+		material = new Material(shader);
+		material.hideFlags = HideFlags.DontSave;
+
+		if (material) return material;
+		else return null;
+	}
+}
