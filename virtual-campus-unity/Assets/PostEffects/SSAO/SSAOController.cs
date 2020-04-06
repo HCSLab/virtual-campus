5d0b9da8807f3ef5a24b863d58e323b9ae1a7863
Tymon <ftm2001@163.com>
2020-04-06 20:35:51 +0800

    demo post effects in scene

--- virtual-campus-unity/Assets/PostEffects/SSAO/SSAOController.cs
+++ virtual-campus-unity/Assets/PostEffects/SSAO/SSAOController.cs
@@ -31,48 +31,51 @@ public class SSAOController : PostEffect
         GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
     }
 
+    void Start()
+    {
+         Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+        float fov = GetComponent<Camera>().fieldOfView;
+        float near = GetComponent<Camera>().nearClipPlane;
+        float aspect = GetComponent<Camera>().aspect;
+
+        float halfHeight = near * Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
+        Vector3 toRight = Vector3.right * halfHeight * aspect;
+        Vector3 toTop = Vector3.up * halfHeight;
+
+        Vector3 topLeft = Vector3.forward * near + toTop - toRight;
+        Vector3 topRight = Vector3.forward * near + toRight + toTop;
+        Vector3 bottomLeft = Vector3.forward * near - toTop - toRight;
+        Vector3 bottomRight = Vector3.forward * near + toRight - toTop;
+
+        bottomLeft /= near;
+        bottomRight /= near;
+        topRight /= near;
+        topLeft /= near;
+
+        frustumCorners.SetRow(0, bottomLeft);
+        frustumCorners.SetRow(1, bottomRight);
+        frustumCorners.SetRow(2, topRight);
+        frustumCorners.SetRow(3, topLeft);
+
+        material.SetMatrix("_FrustumCornersRay", frustumCorners);
+
+        material.SetMatrix("_CameraProjection", GetComponent<Camera>().projectionMatrix);
+
+        var sampleList = new List<Vector4>();
+        for (int i = 0; i < 64; i++)
+        {
+            Vector4 dir = new Vector4(Random.Range(-1f, 1f), Random.Range(0f, 1f), Random.Range(-1f, 1f), 0);
+            dir = Mathf.Pow(Random.Range(0f, 1f), 2) * sampleRadius * dir.normalized;
+            sampleList.Add(dir);
+        }
+        material.SetVectorArray("_SampleList", sampleList);
+    }
+
     void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
         if (material != null && mode != 0)
         {
-            Matrix4x4 frustumCorners = Matrix4x4.identity;
-
-            float fov = GetComponent<Camera>().fieldOfView;
-            float near = GetComponent<Camera>().nearClipPlane;
-            float aspect = GetComponent<Camera>().aspect;
-
-            float halfHeight = near * Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
-            Vector3 toRight = Vector3.right * halfHeight * aspect;
-            Vector3 toTop = Vector3.up * halfHeight;
-
-            Vector3 topLeft = Vector3.forward * near + toTop - toRight;
-            Vector3 topRight = Vector3.forward * near + toRight + toTop;
-            Vector3 bottomLeft = Vector3.forward * near - toTop - toRight;
-            Vector3 bottomRight = Vector3.forward * near + toRight - toTop;
-
-            bottomLeft /= near;
-            bottomRight /= near;
-            topRight /= near;
-            topLeft /= near;
-
-            frustumCorners.SetRow(0, bottomLeft);
-            frustumCorners.SetRow(1, bottomRight);
-            frustumCorners.SetRow(2, topRight);
-            frustumCorners.SetRow(3, topLeft);
-
-            material.SetMatrix("_FrustumCornersRay", frustumCorners);
-
-            material.SetMatrix("_CameraProjection", GetComponent<Camera>().projectionMatrix);
-
-            var sampleList = new List<Vector4>();
-            for (int i = 0; i < 64; i++)
-            {
-                Vector4 dir = new Vector4(Random.Range(-1f, 1f), Random.Range(0f, 1f), Random.Range(-1f, 1f), 0);
-                dir = Mathf.Pow(Random.Range(0f, 1f), 2) * sampleRadius * dir.normalized;
-                sampleList.Add(dir);
-            }
-            material.SetVectorArray("_SampleList", sampleList);
-
             int w = src.width / downSample;
             int h = src.height / downSample;
             RenderTexture buffer = RenderTexture.GetTemporary(w, h, 0);
