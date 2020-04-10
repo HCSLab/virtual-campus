cc364461ffbc98efd6303cc00ab6ca230f6a41af
Tymon <ftm2001@163.com>
2020-04-10 22:18:21 +0800

    correct mouse position and add mouse neighbourhood view

--- virtual-campus-unity/Assets/VoxelMaterialEditor/VoxelMaterialEditorWindow.cs
+++ virtual-campus-unity/Assets/VoxelMaterialEditor/VoxelMaterialEditorWindow.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,74 +11,152 @@ public class VoxelMaterialEditorWindow : EditorWindow
     bool myBool = true;
     float myFloat = 1.23f;
 
+    const int mouseNeighbourhoodSize = 200;
+    static Texture2D mouseNeighbourhoodTex;
+
+    static SceneView sceneView;
+    static Vector2 mousePos;
+
+    private void OnEnable()
+    {
+        SceneView.duringSceneGui += DetectMouseClick;
+        mouseNeighbourhoodTex = new Texture2D(mouseNeighbourhoodSize, mouseNeighbourhoodSize);
+    }
+
     // Add menu item named "My Window" to the Window menu
     [MenuItem("Window/My Window")]
     public static void ShowWindow()
     {
         //Show existing window instance. If one doesn't exist, make one.
         GetWindow(typeof(VoxelMaterialEditorWindow));
-        SceneView.duringSceneGui += DetectMouseClick;
     }
 
-    static void DetectMouseClick(SceneView sceneView)
+    static void DetectMouseClick(SceneView sv)
     {
+        sceneView = sv;
         if (Event.current.type == EventType.MouseDown)
         {
-            Vector3 mousePosition = Event.current.mousePosition;
-            mousePosition.y = sceneView.camera.pixelHeight - mousePosition.y;
-            Debug.Log(mousePosition);
-            OnClick(mousePosition);
+            GUIStyle style = "GV Gizmo DropDown";
+            Vector2 ribbon = style.CalcSize(sceneView.titleContent);
+
+            Vector2 sv_correctSize = sceneView.position.size;
+            sv_correctSize.y -= ribbon.y; //exclude this nasty ribbon
+
+            //flip the position:
+            mousePos = Event.current.mousePosition;
+            //mousePos.y = sceneView.camera.pixelHeight - mousePos.y;
+            mousePos.y = sv_correctSize.y - mousePos.y;
+            mousePos *= sceneView.camera.pixelHeight / sv_correctSize.y;
+            // Debug.Log(mousePos);
+
+            OnClick();
         }
     }
 
-    static void OnClick(Vector2 mousePos)
+    static void OnClick()
     {
         // get selected voxel object and its material and textures
         GameObject obj = Selection.activeGameObject;
         if (obj == null || obj.name != "default") return;
         MeshRenderer render = obj.GetComponent<MeshRenderer>();
         if (render == null) return;
-        Material mat = render.material;
+        Material mat = render.sharedMaterial;
         //if (!mat.HasProperty("_MainTex") || !mat.HasProperty("_PropTex")) return;
         //Texture2D mainTex = (Texture2D)mat.GetTexture("_MainTex");
         //Texture2D propTex = (Texture2D)mat.GetTexture("_PropTex");
         Texture2D propTex = null; // for debug
         if (propTex == null)
         {
-            propTex = newPropTex();
+            propTex = CreatePropTex();
         }
 
         // get the UV at mouse position
-        GameObject camObj = new GameObject();
-        camObj.AddComponent<Camera>();
-        Camera cam = camObj.GetComponent<Camera>();
-        cam.CopyFrom(SceneView.currentDrawingSceneView.camera);
-        var UVTex = RenderTexture.GetTemporary(cam.targetTexture.descriptor);
-        cam.targetTexture = UVTex;
-        Shader UVShader = Shader.Find("Others/RenderUV");
-        cam.RenderWithShader(UVShader, "");
-        RenderTexture.active = UVTex;
-        Texture2D UVTexReader = new Texture2D(1, 1);
-        Rect mouseRect = new Rect(mousePos.x, mousePos.y, 1, 1);
-        UVTexReader.ReadPixels(mouseRect, 0, 0, false);
-        UVTexReader.Apply();
-        Color col = UVTexReader.GetPixel(0, 0);
-        int uv_x = (int)(col.r * 256);
-        RenderTexture.ReleaseTemporary(UVTex);
-        DestroyImmediate(UVTexReader);
-        //cam.SetReplacementShader(UVShader, "");
-        DestroyImmediate(camObj);
+        Texture2D UVTex = DrawWithShaderReplacement(Shader.Find("Others/RenderUV"));
+        Color col = UVTex.GetPixel((int)mousePos.x, (int)mousePos.y);
+        int uv_x = (int)(UVTex.GetPixel((int)mousePos.x, (int)mousePos.y).r * 255);
 
         // draw data into properties texture and save it
         propTex.SetPixel(uv_x, 0, Color.red);
         propTex.Apply();
         var pngData = propTex.EncodeToPNG();
         System.IO.File.WriteAllBytes("./test.png", pngData);
+
+        DrawMouseNighbourhood();
     }
 
-    static Texture2D newPropTex()
+    static void DrawMouseNighbourhood()
     {
-        Texture2D tex = new Texture2D(256, 4, TextureFormat.RGBA32, false);
+        // draw the scene view immediately
+        Texture2D sceneTex = DrawWithShaderReplacement(null);
+
+        // copy the correct region to mouse neighbourhood texture
+        for (int i = 0; i < mouseNeighbourhoodSize; i++)
+        {
+            for (int j = 0; j < mouseNeighbourhoodSize; j++)
+            {
+                int sx = (int)mousePos.x - mouseNeighbourhoodSize / 2 + i;
+                int sy = (int)mousePos.y - mouseNeighbourhoodSize / 2 + j;
+                if (sx >= 0 && sy >= 0 && sx < sceneTex.width && sy < sceneTex.height)
+                {
+                    mouseNeighbourhoodTex.SetPixel(i, j, sceneTex.GetPixel(sx, sy));
+                }
+                else
+                {
+                    mouseNeighbourhoodTex.SetPixel(i, j, Color.black);
+                }
+            }
+        }
+
+        // draw red reference line
+        for (int i = 0; i < mouseNeighbourhoodSize; i++)
+        {
+            mouseNeighbourhoodTex.SetPixel(mouseNeighbourhoodSize / 2, i, Color.red);
+            mouseNeighbourhoodTex.SetPixel(i, mouseNeighbourhoodSize / 2, Color.red);
+        }
+
+        // apply changes
+        mouseNeighbourhoodTex.Apply();
+    }
+
+    static Texture2D DrawWithShaderReplacement(Shader shader, string replacementTag = "")
+    {
+        // create a new camera and place it as the scene view camera
+        GameObject camObj = new GameObject();
+        camObj.AddComponent<Camera>();
+        Camera cam = camObj.GetComponent<Camera>();
+        cam.CopyFrom(sceneView.camera);
+
+        // create a temporary render texture and use it as the target
+        var tempRT = RenderTexture.GetTemporary(cam.targetTexture.descriptor);
+        cam.targetTexture = tempRT;
+        if (shader != null)
+        {
+            // draw with shader replacement
+            cam.RenderWithShader(shader, replacementTag);
+        }
+        else
+        {
+            // shader is null, draw with original materials
+            cam.Render();
+        }
+
+        // read from render texture to a 2d texture
+        RenderTexture.active = tempRT;
+        Texture2D tex = new Texture2D(tempRT.width, tempRT.height);
+        tex.ReadPixels(new Rect(0, 0, tempRT.width, tempRT.height), 0, 0);
+        tex.Apply();
+
+        // clean up
+        RenderTexture.active = null;
+        RenderTexture.ReleaseTemporary(tempRT);
+        DestroyImmediate(camObj);
+
+        return tex;
+    }
+
+    static Texture2D CreatePropTex()
+    {
+        Texture2D tex = new Texture2D(255, 4, TextureFormat.RGBA32, false);
         tex.filterMode = FilterMode.Point;
 
         for (int i = 0; i < 256; i++)
@@ -90,6 +169,7 @@ public class VoxelMaterialEditorWindow : EditorWindow
 
     void OnGUI()
     {
+        GUILayout.Label(mouseNeighbourhoodTex);
         GUILayout.Label("Base Settings", EditorStyles.boldLabel);
         myString = EditorGUILayout.TextField("Text Field", myString);
 
@@ -104,8 +184,14 @@ public class VoxelMaterialEditorWindow : EditorWindow
         if (myFloat < 0) myFloat = -myFloat;
     }
 
+    private void Update()
+    {
+        Repaint();
+    }
+
     private void OnDestroy()
     {
         SceneView.duringSceneGui -= DetectMouseClick;
+        DestroyImmediate(mouseNeighbourhoodTex);
     }
 }
