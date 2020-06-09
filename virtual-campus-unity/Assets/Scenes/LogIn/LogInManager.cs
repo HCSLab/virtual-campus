28fc042df275dbb2933dd204e9445da548914bd0
Okami Wong <wtfimba@live.com>
2020-06-09 15:02:46 +0800

    add notification center to LogIn scene

---
+++ virtual-campus-unity/Assets/Scenes/LogIn/LogInManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LogInManager : MonoBehaviour
+{
+	enum LogInResult
+	{
+		Success,
+		WrongPasswordOrUsername,
+		InternetError
+	};
+
+	public GameObject logInButton;
+	public void LogIn()
+	{
+		LogInNotificationCenter.instance.Remove(LogInNotificationCenter.NotificationType.WrongPasswordOrUsername);
+		LogInNotificationCenter.instance.Remove(LogInNotificationCenter.NotificationType.InternetError);
+
+		logInButton.GetComponent<Button>().interactable = false;
+
+		LogInNotificationCenter.instance.Post(LogInNotificationCenter.NotificationType.LoggingIn);
+
+		var result = LogInResult.InternetError;
+
+		switch (result)
+		{
+			case LogInResult.Success:
+				LeanTween.delayedCall(2f, () =>
+				{
+					LogInNotificationCenter.instance.Remove(LogInNotificationCenter.NotificationType.LoggingIn);
+					//SceneManager.LoadScene("NewStartUpZone");
+				});
+				break;
+			case LogInResult.WrongPasswordOrUsername:
+				LeanTween.delayedCall(2f, () =>
+				{
+					LogInNotificationCenter.instance.Remove(LogInNotificationCenter.NotificationType.LoggingIn);
+					LogInNotificationCenter.instance.Post(LogInNotificationCenter.NotificationType.WrongPasswordOrUsername);
+					logInButton.GetComponent<Button>().interactable = true;
+				});
+				break;
+			case LogInResult.InternetError:
+				LeanTween.delayedCall(2f, () =>
+				{
+					LogInNotificationCenter.instance.Remove(LogInNotificationCenter.NotificationType.LoggingIn);
+					LogInNotificationCenter.instance.Post(LogInNotificationCenter.NotificationType.InternetError);
+					logInButton.GetComponent<Button>().interactable = true;
+				});
+				break;
+		}
+	}
+}
