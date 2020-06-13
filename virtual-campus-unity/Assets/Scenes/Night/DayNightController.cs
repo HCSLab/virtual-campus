75e3fb351ff675dc2c25203e5579ef9979861bd6
Tymon <ftm2001@163.com>
2020-06-13 23:14:01 +0800

    day night switch

---
+++ virtual-campus-unity/Assets/Scenes/Night/DayNightController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightController : MonoBehaviour
+{
+    public GameObject dayModel;
+    public GameObject nightModel;
+
+    public void OoNight()
+    {
+        dayModel.SetActive(false);
+        nightModel.SetActive(true);
+    }
+
+    public void OnDay()
+    {
+        dayModel.SetActive(true);
+        nightModel.SetActive(false);
+    }
+}
