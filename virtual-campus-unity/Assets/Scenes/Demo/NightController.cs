74b162961ae46d1dcaadcfeb94b9afe18b0c0d1f
Okami Wong <wtfimba@live.com>
2020-06-14 23:25:58 +0800

    update the transition between day and night and let the builded game start with Demo and DemoNight

---
+++ virtual-campus-unity/Assets/Scenes/Demo/NightController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class NightController : MonoBehaviour
+{
+	public GameObject dayWorld;
+    public void OnLightMapChange()
+	{
+		dayWorld.SetActive(false);
+		GameObject.FindGameObjectWithTag("NightTimeline").GetComponent<PlayableDirector>().Play();
+	}
+}
