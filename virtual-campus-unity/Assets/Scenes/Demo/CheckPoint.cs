1ccd22132ae346c124a376b0cfcabb517e4e7940
Okami Wong <wtfimba@live.com>
2020-06-14 17:04:22 +0800

    import new SimpleCitizens and set up day and night transition in the Demo scene

---
+++ virtual-campus-unity/Assets/Scenes/Demo/CheckPoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+	public Color gizmoColor;
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = gizmoColor;
+		Gizmos.DrawSphere(transform.position, 0.1f);
+	}
+}
