92d1ea73807a5cf42fe5dc15a82732dde69adc74
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-07-24 16:37:50 +0800

    Add Current Region

---
+++ virtual-campus-unity/Assets/Scripts/Map/CurrentRegion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentRegion : MonoBehaviour
+{
+    private Transform player;
+    public TMPro.TextMeshProUGUI tm;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        bool inRegion = Physics.Raycast(player.position, Vector3.down, out hit, 100.0f, 1 << LayerMask.NameToLayer("CurrentRegion"));
+        if (inRegion)
+        {
+            string regionName = hit.collider.gameObject.GetComponent<RegionQuad>().name;
+            tm.text = regionName;
+        }
+        else
+        {
+            tm.text = "下园";
+        }
+    }
+}
---
+++ virtual-campus-unity/Assets/Scripts/Map/MinimapCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapCamera : MonoBehaviour
+{
+    protected Camera minimapCamera;
+    //private Transform minimapQuad;
+    protected Transform player;
+    private Vector3 offsetPosition;
+    // Use this for initialization
+    void Start()
+    {
+        minimapCamera = GetComponent<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        offsetPosition = minimapCamera.transform.position - player.position;
+        //minimapQuad = GameObject.FindGameObjectWithTag("Player").GetComponent<MinimapQuad>().transform;
+    }
+
+    void Update()
+    {
+        minimapCamera.transform.position = offsetPosition + player.position;
+    }
+
+    public void ZoomInButtonClick()
+    {
+        if (minimapCamera.orthographicSize > 6)
+        {
+            minimapCamera.orthographicSize -= 3;
+            //minimapQuad.localScale /= 2;
+        }
+    }
+
+    public void ZoomOutButtonClick()
+    {
+        if (minimapCamera.orthographicSize < 27)
+        {
+            minimapCamera.orthographicSize += 3;
+            //minimapQuad.localScale *= 2;
+        }
+    }
+}
