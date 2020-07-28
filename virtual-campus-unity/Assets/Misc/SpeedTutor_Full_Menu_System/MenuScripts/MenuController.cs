7dacceceb9340c4f1a462880612de73689c52dec
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-07-28 18:52:51 +0800

    Update main menu

--- virtual-campus-unity/Assets/Misc/SpeedTutor_Full_Menu_System/MenuScripts/MenuController.cs
+++ virtual-campus-unity/Assets/Misc/SpeedTutor_Full_Menu_System/MenuScripts/MenuController.cs
@@ -33,6 +33,8 @@ namespace SpeedTutorMainMenuSystem
         [SerializeField] private GameObject controlsMenu;
         [SerializeField] private GameObject confirmationMenu;
         [SerializeField] private GameObject loadingScreen;
+        public Image progressBar;
+        List<AsyncOperation> scenesLoading = new List<AsyncOperation>();
         [Space(10)]
         [Header("Menu Popout Dialogs")]
         [SerializeField] private GameObject noSaveDialog;
@@ -243,13 +245,50 @@ namespace SpeedTutorMainMenuSystem
             }
         }
         #endregion
+        IEnumerator GetSceneLoadProgress()
+        {
+            foreach (AsyncOperation op in scenesLoading)
+            {
+                if (op == null)
+                {
+                    continue;
+                }
+                while (!op.isDone)
+                {
+                    var totalProgress = 0f;
+                    foreach (AsyncOperation op2 in scenesLoading)
+                    {
+                        if (op2 != null)
+                            totalProgress += op2.progress;
+                    }
+                        
+                    totalProgress /= scenesLoading.Count;
+                    progressBar.fillAmount = totalProgress;
+                    yield return null;
+                }
+            }
+
+            loadingScreen.SetActive(false);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex((int)SceneIndexes.MainGame));
+        }
+
+        public void LoadGame()
+        {
+            loadingScreen.SetActive(true);
+
+            scenesLoading.Add(SceneManager.UnloadSceneAsync((int)SceneIndexes.MainMenu));
+            scenesLoading.Add(SceneManager.LoadSceneAsync((int)SceneIndexes.MainGame, LoadSceneMode.Additive));
+
+            StartCoroutine(GetSceneLoadProgress());
+        }
 
         #region Dialog Options - This is where we load what has been saved in player prefs!
         public void ClickNewGameDialog(string ButtonType)
         {
             if (ButtonType == "Yes")
             {
-                SceneManager.LoadScene(_newGameButtonLevel);
+                //SceneManager.LoadScene(_newGameButtonLevel);
+                LoadGame();
             }
 
             if (ButtonType == "No")
