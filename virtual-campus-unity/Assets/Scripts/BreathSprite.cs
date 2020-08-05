901f501ecc3d627d22f0f1a637513e9674597a4b
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-08-06 02:46:17 +0800

    Update Big Map

---
+++ virtual-campus-unity/Assets/Scripts/BreathSprite.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreathSprite : MonoBehaviour
+{
+
+    public SpriteRenderer sr;
+    public float speed;
+    public float maxAlpha;
+    public float minAlpha;
+
+    bool increase = true;
+
+
+    void Update()
+    {
+        if (increase)
+        {
+            Color c = sr.color;
+            c.a += speed;
+            if (c.a > maxAlpha)
+            {
+                c.a = maxAlpha;
+                increase = false;
+            }
+            sr.color = c;
+        }
+        else
+        {
+            Color c = sr.color;
+            c.a -= speed;
+            if (c.a < minAlpha)
+            {
+                c.a = minAlpha;
+                increase = true;
+            }
+            sr.color = c;
+        }
+    }
+}
