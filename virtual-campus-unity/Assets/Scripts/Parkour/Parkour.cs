04dcfaa5c8038b1c523b80d008dbe0ca90306b15
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2020-08-11 23:50:28 +0800

    Add Parkour

---
+++ virtual-campus-unity/Assets/Scripts/Parkour/Parkour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parkour : MonoBehaviour
+{
+    public List<GameObject> pathPoints;
+    public Timer timer;
+    public float timeLimit;
+    [HideInInspector]
+    public int nextPathPoint;
+
+    void Start()
+    {
+        
+    }
+
+    public void StartParkour()
+    {
+        timer.StartTiming(timeLimit);
+        nextPathPoint = 0;
+        pathPoints[0].SetActive(true);
+    }
+
+    public void NextPathPoint()
+    {
+        pathPoints[nextPathPoint].SetActive(false);
+        nextPathPoint++;
+        pathPoints[nextPathPoint].SetActive(true);
+    }
+    public void Success()
+    {
+        
+    }
+
+    public void Fail()
+    {
+
+    }
+    
+    void Update()
+    {
+        //if (pathPoints[nextPathPoint].GetComponent<BoxCollider>())
+    }
+}
