c8ff9f82ece716e8d8bb0204fbd82ae2564fa5d9
Okami Wong <wtfimba@live.com>
2020-08-14 22:06:11 +0800

    Disable logging in the first three frames
    Because loading will generate duplicated logs

--- virtual-campus-unity/Assets/Scripts/UI/LogNotificationCenter.cs
+++ virtual-campus-unity/Assets/Scripts/UI/LogNotificationCenter.cs
@@ -10,13 +10,27 @@ public class LogNotificationCenter : MonoBehaviour
 	public GameObject notificationPrefab;
 	public float displayDuration;
 
+	bool isLoggingEnabled = false;
+
 	private void Awake()
 	{
 		Instance = this;
+		StartCoroutine(EnableLoggingCoroutine());
+	}
+
+	IEnumerator EnableLoggingCoroutine()
+	{
+		yield return null;
+		yield return null;
+		yield return null;
+		isLoggingEnabled = true;
 	}
 
 	public void Post(string content)
 	{
+		if (!isLoggingEnabled)
+			return;
+
 		var notification = Instantiate(notificationPrefab);
 		notification.transform.SetParent(gameObject.transform);
 		notification.transform.localScale = Vector3.one * 0.8f;
@@ -31,7 +45,8 @@ public class LogNotificationCenter : MonoBehaviour
 
 		LeanTween.delayedCall(
 			displayDuration,
-			() => {
+			() =>
+			{
 				notification.LeanScale(Vector3.one * 0.8f, 0.2f).setOnComplete(() => { Destroy(notification); });
 			}
 			);
