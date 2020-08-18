7765923ac134d2ba9cf6a2b3fac48aebea5e8913
Okami Wong <wtfimba@live.com>
2020-08-18 13:53:36 +0800

    some performance improvement

--- virtual-campus-unity/Assets/Scripts/Map/CurrentRegion.cs
+++ virtual-campus-unity/Assets/Scripts/Map/CurrentRegion.cs
@@ -5,47 +5,52 @@ using UnityEngine.UI;
 
 public class CurrentRegion : MonoBehaviour
 {
-    public static CurrentRegion Instance;
+	public static CurrentRegion Instance;
 
-    public TMPro.TextMeshProUGUI tm;
+	public TMPro.TextMeshProUGUI tm;
 
-    [HideInInspector]
-    public int regionCount;
+	[HideInInspector]
+	public int regionCount;
 
-    Transform player;
-    RegionQuad[] regions;
+	Transform player;
+	RegionQuad[] regions;
 
 	private void Awake()
 	{
-        Instance = this;
+		Instance = this;
 
-        regions = GameObject.FindObjectsOfType<RegionQuad>();
-        regionCount = regions.Length;
-    }
+		regions = GameObject.FindObjectsOfType<RegionQuad>();
+		regionCount = regions.Length;
+	}
 
 	void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-    }
+	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
 
-    void Update()
-    {
-        RaycastHit hit;
-        bool inRegion = Physics.Raycast(player.position, Vector3.down, out hit, 100.0f, 1 << LayerMask.NameToLayer("CurrentRegion"));
-        if (inRegion)
-        {
-            RegionQuad regionQuad = hit.collider.gameObject.GetComponent<RegionQuad>();
-            if (!regionQuad.isVisited)
-            {
-                regionQuad.isVisited = true;
-                EventCenter.Broadcast(EventCenter.AchievementEvent.NewAreaExplored, null);
-            }
-            string regionName = regionQuad.name;
-            tm.text = regionName;
-        }
-        else
-        {
-            tm.text = "下园";
-        }
-    }
+	int frameCount = 0;
+	void Update()
+	{
+		frameCount = (frameCount + 1) % 15;
+		if (frameCount != 0)
+			return;
+
+		RaycastHit hit;
+		bool inRegion = Physics.Raycast(player.position, Vector3.down, out hit, 100.0f, 1 << LayerMask.NameToLayer("CurrentRegion"));
+		if (inRegion)
+		{
+			RegionQuad regionQuad = hit.collider.gameObject.GetComponent<RegionQuad>();
+			if (!regionQuad.isVisited)
+			{
+				regionQuad.isVisited = true;
+				EventCenter.Broadcast(EventCenter.AchievementEvent.NewAreaExplored, null);
+			}
+			string regionName = regionQuad.name;
+			tm.text = regionName;
+		}
+		else
+		{
+			tm.text = "下园";
+		}
+	}
 }
